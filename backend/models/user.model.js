a5ea22cb43ec3bb26dfeb61774a498d09bf2f2bb
bijoymandal <bijoy.mandal@mydevteam.com>
2025-09-16 22:56:52 +0530

    Users  collection details and user Models

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
 const {Schema} = mongoose;
 const userSchema = new Schema({
     name: {type:String,required:true},
@@ -7,5 +9,30 @@ const userSchema = new Schema({
     role: {type:String,enum:['student','instructor','admin'],default:"student"},
 });
 
+// Pre-save hook to hash password before saving
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+
+    try{
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password,salt);
+        next();
+    }
+    catch(error)
+    {
+        console.log(error);
+        next(error);
+    }
+});
+
+//compare entered password with hashed password
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+};
+
+
+
 const User = mongoose.model("User",userSchema);
+
+
 export default User;
