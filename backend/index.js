464fb8632312b82a287d9a6c06222a3871afe3ca
bijoymandal <bijoy.mandal@mydevteam.com>
2025-09-20 00:09:29 +0530

    All roues added in main "index.js"end point API file

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ dotenv.config();
 import connectionDB from './config/db.js';
 import Authrouter from './routes/auth.routes.js';
 import swagger from "swagger-ui-express";
+import Courserouter from "./routes/course.routes.js";
+import Categoryrouter from "./routes/category.routes.js";
 
 const apiDocs = JSON.parse(
   fs.readFileSync(new URL("./swagger.json", import.meta.url), "utf-8")
@@ -29,7 +31,11 @@ connectionDB();
 if(process.env.NODE_ENV === 'dev'){
   server.use(morgan('dev'));
 }
-server.use('/api-docs', swagger.serve, swagger.setup(apiDocs));
+server.use('/api-docs', swagger.serve, swagger.setup(apiDocs)); // swagger api endpoint
+
+server.use('/api/auth',Authrouter); // authentication 
+server.use('/api/course',Courserouter);
+server.use('/api/category',Categoryrouter);
 
 
 server.get('/', (req, res) => {
