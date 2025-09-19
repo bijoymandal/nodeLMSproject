872ee77286efaff4b4b6995cbd738de0a475d9a3
bijoymandal <bijoy.mandal@mydevteam.com>
2025-09-19 23:40:39 +0530

    Database base connection changes to update time of connection database

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,12 @@ dotenv.config();
 
 const connectionDB = async()=>{
     try{
-        const connection = await mongoose.connect(process.env.MONGO_URI);
+        const connection = await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000
+        });
+        
         console.log(`MongoDB connected: ${connection.connection.host}`);
     }
     catch(err){
